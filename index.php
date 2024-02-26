fe5929dcd7150d7920ea95d0b838d108520b62b5
Priya Pradhan <priy437pradhan@gmail.com>
2024-02-26 15:25:03 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,528 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Hindustan-media</title>
+      <style>
+         * {
+         list-style-type: none;
+         }
+         body {
+         margin: 0;
+         padding: 0;
+         font-family: "Roboto", sans-serif;
+         }
+         section.navbar-hm {
+         display: flex;
+         justify-content: space-around;
+         width: 100%;
+         height: auto;
+         background-color: #fff;
+         border-bottom: .5px solid #A8A8A8;
+         align-items: center;
+         }
+         img.logo-hm {
+         height: 65px;
+         width: 250px;
+         }
+         ul.more-icons-hm {
+         display: block;
+         }
+         ul.more-icons-hm li {
+         display: inline-block;
+         }
+         .flex-menu-container {
+         display: flex;
+         background-color: #fff;
+         color: #333;
+         }
+         .static-menu {
+         padding: 10px;
+         background-color: #eee;
+         width: 100px;
+         height: 40px;
+         }
+         ul.scrolling-menu {
+         list-style-type: none;
+         margin: 0;
+         padding: 0;
+         display: flex;
+         overflow-x: auto;
+         }
+         ul.scrolling-menu li {
+         padding: 5px;
+         margin-right: 5px;
+         border-radius: 5px;
+         background-color: #fff;
+         display: flex;
+         align-items: center;
+         }
+         ul.scrolling-menu li img {
+         width: 15px;
+         height: 15px;
+         margin-right: 5px;
+         border-radius: 50%;
+         }
+         /* live-section */
+         .head-news {
+         padding: 10px;
+         border-bottom:2px solid #A8A8A8;
+         }
+         h1.headline-container {
+         font-size: 24px;
+         color: #333;
+         line-height: 34px;
+         margin-bottom: 10px 
+         }
+         .live-button {
+         background-color: #FF004D;
+         color: #fff;
+         padding: 5px 10px;
+         text-decoration: none;
+         border-radius: 5px;
+         }
+         .headline-container-img {
+         width: 100%;
+         height: auto;
+         border-radius: 10px;
+         }
+         .img-container-hm {
+         position: relative;
+         display: inline-block;
+         }
+         .headline-container-img {
+         max-width: 100%;
+         height: auto;
+         display: block;
+         }
+         .play-icon-hm {
+         position: absolute;
+         z-index: 1;
+         left: 50%;
+         top: 50%;
+         transform: translate(-50%, -50%);
+         height: 40px;
+         width: 40px;
+         }
+         .img-container-hm span {
+         position: absolute;
+         right: 10px;
+         color: #fff;
+         background-color: #555555c2;
+         padding: 5px;
+         border-radius: 10px;
+         font-size: 14px;
+         z-index: 1;
+         bottom: 10px;
+         }
+         .container-element {
+         display: flex;
+         justify-content: space-between;
+         padding: 10px;
+         border-radius: 10px;
+         }
+         p.left-element  {
+         color:#555;
+         font-weight:bold;
+         font-size: 16px;
+         margin: 0;
+         }
+         ul.right-element{
+         list-style-type: none;
+         margin: 0;
+         padding: 0;
+         display: flex;
+         }
+         ul.right-element li {
+         margin-right: 10px;
+         }
+         ul.right-element li img {
+         width: 20px;
+         height: 20px;
+         margin-right: 5px;
+         border-radius: 50%;
+         }
+         /* live-section */
+         /* type-one-news  */
+         .type-one-news {
+         padding: 10px;
+         border-bottom:1px solid #A8A8A8;
+
+         }
+         .one-news-img{
+         position: relative;
+         }
+         h3.headline-container-TO {
+         font-size: 18px;
+         color: #333;
+         line-height: 28px;
+         padding-right:160px;
+         }
+         
+         .img-container-TO {
+         position: absolute;
+         right: 0;
+         width: 160px;
+         top: 0;
+         display: inline-block;
+         }
+         .headline-container-img {
+         max-width: 100%;
+         height: auto;
+         display: block;
+         }
+         .play-icon-TO {
+         position: absolute;
+         z-index: 1;
+         left: 50%;
+         top: 50%;
+         transform: translate(-50%, -50%);
+         height: 20px;
+         width: 20px;
+         }
+         .img-container-TO span {
+         position: absolute;
+         right: 10px;
+         color: #fff;
+         background-color: #555555c2;
+         padding: 5px;
+         border-radius: 10px;
+         font-size: 12px;
+         z-index: 1;
+         bottom: 10px;
+         }
+         /* type-one-news  */
+         /* type-two-news */
+         .two-news-img{
+         position: relative;
+         border-bottom:1px solid #A8A8A8;
+
+
+         }
+         p.headline-container-TW {
+         font-size: 18px;
+         color: #000;
+         line-height: 28px;
+         margin:2px;
+         border-bottom:1px solid #A8A8A8;
+         }
+         
+         .img-container-TW {
+         position: absolute;
+         right: 0;
+         width: 160px;
+         top: 0;
+         display: inline-block;
+         }
+        
+         .play-icon-TW {
+         position: absolute;
+         z-index: 1;
+         left: 50%;
+         top: 50%;
+         transform: translate(-50%, -50%);
+         height: 20px;
+         width: 20px;
+         }
+         .img-container-TW span {
+         position: absolute;
+         right: 10px;
+         color: #fff;
+         background-color: #555555c2;
+         padding: 5px;
+         border-radius: 10px;
+         font-size: 12px;
+         z-index: 1;
+         bottom: 10px;
+         }
+         /* type-two-news */
+         /* single-card-container */
+         .photos-card-container{
+            display:flex;
+            width: auto;
+            overflow-x:scroll;
+         }
+         .photo-card-single {
+    display: block;
+    min-width: 280px;
+    height: auto;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    margin-right: 10px;
+}
+         .photo-card-single img{
+            width: 100%;
+            border-radius:20px 20px 0 0;
+            box-shadow: 0 2px 4px rgba(0,0,0,0.2)
+         }
+         .photo-card-single h3{
+            margin:10px 20px;
+         }
+         /* single-card-container */
+      </style>
+   </head>
+   <body>
+      <section class="navbar-hm">
+         <img src="https://odishatv.in/commondir/images/logo.svg" class="main-logo-img" alt="OdishaTV" title="OdishaTV" style="width: 250px; height: 65px;">
+         <ul class="more-icons-hm">
+            <li>dd</li>
+            <li>dd</li>
+         </ul>
+      </section>
+      <section class="flex-menu-container">
+         <div class="static-menu">
+            <p>Static</p>
+         </div>
+         <ul class="scrolling-menu">
+            <li>
+               <img src="path/to/image1.jpg">
+               Item 1
+            </li>
+            <li>
+               <img src="path/to/image1.jpg">
+               Item 1
+            </li>
+            <li>
+               <img src="path/to/image2.jpg">
+               Item 2
+            </li>
+            <li>
+               <img src="path/to/image2.jpg">
+               Item 2
+            </li>
+            <li>
+               <img src="path/to/image2.jpg">
+               Item 2
+            </li>
+            <li>
+               <img src="path/to/image2.jpg">
+               Item 2
+            </li>
+            <li>
+               <img src="path/to/image2.jpg">
+               Item 2
+            </li>
+            <li>
+               <img src="path/to/image2.jpg">
+               Item 2
+            </li>
+         </ul>
+      </section>
+      <section class="head-news">
+         <h1 class="headline-container"> <a href="#" class="live-button">Live</a> Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h1>
+         <div class="img-container-hm">
+            <img src="images/default.jpg" alt="" class="headline-container-img">
+            <img src="images/video-icon-red.svg" alt="" class="play-icon-hm">
+            <span>11:30</span>
+         </div>
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </section>
+      <!-- type-one-news -->
+      <div class="type-one-news">
+         <div class="one-news-img">
+            <h3 class="headline-container-TO">  Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h3>
+            <div class="img-container-TO">
+               <img src="images/default.jpg" alt="" class="headline-container-img">
+               <img src="images/video-icon-red.svg" alt="" class="play-icon-TO">
+               <span>11:30</span>
+            </div>
+         </div>
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </div>
+      <div class="type-one-news">
+         <div class="one-news-img">
+            <h3 class="headline-container-TO"> <a href="#" class="live-button">Live</a> Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h3>
+            <div class="img-container-TO">
+               <img src="images/default.jpg" alt="" class="headline-container-img">
+               <img src="images/video-icon-red.svg" alt="" class="play-icon-TO">
+               <span>11:30</span>
+            </div>
+         </div>
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </div>
+      <div class="type-one-news">
+         <div class="one-news-img">
+            <h3 class="headline-container-TO">  Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h3>
+            <div class="img-container-TO">
+               <img src="images/default.jpg" alt="" class="headline-container-img">
+               <img src="images/video-icon-red.svg" alt="" class="play-icon-TO">
+               <span>11:30</span>
+            </div>
+         </div>
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </div>
+      <div class="type-one-news">
+         <div class="one-news-img">
+            <h3 class="headline-container-TO">  Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h3>
+            <div class="img-container-TO">
+               <img src="images/default.jpg" alt="" class="headline-container-img">
+               <img src="images/video-icon-red.svg" alt="" class="play-icon-TO">
+               <span>11:30</span>
+            </div>
+         </div>
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </div>
+      <div class="type-one-news">
+         <div class="one-news-img">
+            <h3 class="headline-container-TO">  Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h3>
+            <div class="img-container-TO">
+               <img src="images/default.jpg" alt="" class="headline-container-img">
+               <img src="images/video-icon-red.svg" alt="" class="play-icon-TO">
+               <span>11:30</span>
+            </div>
+         </div>
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </div>
+      <div class="type-one-news">
+         <div class="one-news-img">
+            <h3 class="headline-container-TO">  Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h3>
+            <div class="img-container-TO">
+               <img src="images/default.jpg" alt="" class="headline-container-img">
+               <img src="images/video-icon-red.svg" alt="" class="play-icon-TO">
+               <span>11:30</span>
+            </div>
+         </div>
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </div>
+      <!-- type-one-news -->
+      <!-- type-two-news -->
+      <div class="type-one-news">
+        <h2>Education</h2>
+         <div class="two-news-img">
+            <h3 class="headline-container-TO"> Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</h3>
+            <div class="img-container-TW">
+               <img src="images/default.jpg" alt="" class="headline-container-img">
+              
+            </div>
+         </div>
+            <p class="headline-container-TW"> Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</p>
+            <p class="headline-container-TW"> Lorem ipsum dolor sit amet tetur adipi elit. Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</p>
+            <p class="headline-container-TW"> Lorem ipsum dolor sit amet  Exercitationem aliquam sunt eum, fugiat henderit id aperiam.</p>
+           
+         
+
+         <div class="container-element">
+            <p class="left-element">INDIA</p>
+            <ul class="right-element">
+               <li>
+                  <img src="path/to/image2.jpg">
+                  save
+               </li>
+               <li>
+                  <img src="path/to/image2.jpg">
+                  share
+               </li>
+            </ul>
+         </div>
+      </div>
+      <!-- type-two-news -->
+      <!-- photo-card-section -->
+      <div class="type-one-news">
+         <h2>Photos</h2>
+         <div class="photos-card-container">
+            <div class="photo-card-single">
+               <img src="images/default.jpg" alt="">
+               <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt repellendus soluta maxime.</h3>
+            </div>
+            <div class="photo-card-single">
+               <img src="images/default.jpg" alt="">
+               <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt repellendus soluta maxime.</h3>
+            </div>
+            <div class="photo-card-single">
+               <img src="images/default.jpg" alt="">
+               <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt repellendus soluta maxime.</h3>
+            </div>
+            <div class="photo-card-single">
+               <img src="images/default.jpg" alt="">
+               <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt repellendus soluta maxime.</h3>
+            </div>
+         </div>
+      </div>
+      <!-- photo-card-section -->
+   </body>
+</html>
